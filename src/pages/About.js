fad55690beac6bdfa663e8921e556613e4779e94
Binyam Sintayehu <73512283+Binyam-maker@users.noreply.github.com>
2022-06-06 09:53:24 +0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia illum,
+        sit architecto expedita debitis modi eum eos accusantium. Officiis omnis
+        at, dolore impedit nisi quasi a laboriosam nemo molestiae consequuntur,
+        fugiat ut maiores ipsum, quae harum alias blanditiis maxime ex.
+      </p>
+    </section>
+  );
+};
+
+export default About;
